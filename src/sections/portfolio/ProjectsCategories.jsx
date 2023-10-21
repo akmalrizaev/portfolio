2aced341554ddf6e7f4a1b01093e956fef897738
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-10-21 18:01:21 +0500

    Category Button component

--- src/sections/portfolio/ProjectsCategories.jsx
+++ src/sections/portfolio/ProjectsCategories.jsx
@@ -5,7 +5,11 @@ const ProjectsCategories = ({ categories }) => {
   return (
     <div className="portfolio__categories">
       {categories.map((category) => (
-        <CategoryButton key={category} category={category} />
+        <CategoryButton
+          key={category}
+          category={category}
+          className={`btn cat__btn white`}
+        />
       ))}
     </div>
   );
