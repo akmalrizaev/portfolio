1382bbdd7df5484f401a0493c0d4106722868b12
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-10-21 17:50:13 +0500

    Projects Categories component

--- src/sections/portfolio/ProjectsCategories.jsx
+++ src/sections/portfolio/ProjectsCategories.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import CategoryButton from './CategoryButton';
 
-const ProjectsCategories = () => {
-  return <div>ProjectsCategories</div>;
+const ProjectsCategories = ({ categories }) => {
+  return (
+    <div className="portfolio__categories">
+      {categories.map((category) => (
+        <CategoryButton key={category} category={category} />
+      ))}
+    </div>
+  );
 };
 
 export default ProjectsCategories;
