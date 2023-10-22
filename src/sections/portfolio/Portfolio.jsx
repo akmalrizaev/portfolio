4c83f99f2e8f19855746d628f5060deea4cbe3d8
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-10-22 08:08:53 +0500

    Filter Projects functionality

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -10,6 +10,18 @@ const Portfolio = () => {
   const categories = data.map((item) => item.category);
   const uniqueCategories = ['all', ...new Set(categories)];
 
+  const filterProjectsHandler = (category) => {
+    if (category === 'all') {
+      setProjects(data);
+      return;
+    }
+
+    const filterProjects = data.filter(
+      (project) => project.category === category
+    );
+    setProjects(filterProjects);
+  };
+
   return (
     <section id="portfolio">
       <h2>Recent Projects</h2>
@@ -18,7 +30,10 @@ const Portfolio = () => {
         the buttons to toggle the different categories.
       </p>
       <div className="container portfolio__container">
-        <ProjectsCategories categories={uniqueCategories} />
+        <ProjectsCategories
+          categories={uniqueCategories}
+          onFilterProjects={filterProjectsHandler}
+        />
         <Projects projects={projects} />
       </div>
     </section>
