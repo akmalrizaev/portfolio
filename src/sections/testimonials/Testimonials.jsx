81636a38131cf902556999e5bc9c659a739f48f9
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-10-22 09:52:51 +0500

    Creating Carousel Slides with Swiper JS

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,3 +1,18 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/autoplay';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+import testimonials from './data';
+
+import Testimonial from './Testimonial';
+
 import './testimonials.css';
 
 const Testimonials = () => {
@@ -5,7 +20,28 @@ const Testimonials = () => {
     <section id="testimonials">
       <h2>What My Clients Say</h2>
       <p>These are unbiased testmonials from some of my clients</p>
-      <div className="container"></div>
+      <div className="container">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          breakpoints={{
+            601: { slidesPerView: 2 },
+            1025: { slidesPerView: 3 },
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination, Autoplay]}
+          autoplay={true}
+          className="mySwiper"
+        >
+          {testimonials.map((testimonial) => (
+            <SwiperSlide key={testimonial.id}>
+              <Testimonial testimonial={testimonial} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </section>
   );
 };
