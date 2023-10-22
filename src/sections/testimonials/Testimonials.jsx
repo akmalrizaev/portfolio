212c1d9d0d475c15bbbb4c54a81cf3dc96bb41da
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-10-22 09:28:59 +0500

    Testimonials component

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -3,7 +3,9 @@ import './testimonials.css';
 const Testimonials = () => {
   return (
     <section id="testimonials">
-      <h2>Testimonials</h2>
+      <h2>What My Clients Say</h2>
+      <p>These are unbiased testmonials from some of my clients</p>
+      <div className="container"></div>
     </section>
   );
 };
