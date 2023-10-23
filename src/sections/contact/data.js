76c2fc290572e5b268bb2cc500f68be5f1329115
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-10-23 09:25:54 +0500

    styling Contact component

--- src/sections/contact/data.js
+++ src/sections/contact/data.js
@@ -1,6 +1,7 @@
 import { HiOutlineMail } from 'react-icons/hi';
 import { RiMessengerLine } from 'react-icons/ri';
 import { FaWhatsapp } from 'react-icons/fa';
+import { LiaTelegram } from 'react-icons/lia';
 
 const data = [
   { id: 1, icon: <HiOutlineMail />, link: 'mailto:akmaljon.rizayev@gmail.com' },
@@ -10,6 +11,7 @@ const data = [
     link: 'https://www.messenger.com/t/1000872178800',
   },
   { id: 3, icon: <FaWhatsapp />, link: 'https://wa.me/+2335570975' },
+  { id: 4, icon: <LiaTelegram />, link: 'https://wa.me/+2335570975' },
 ];
 
 // alternative whatsApp link
