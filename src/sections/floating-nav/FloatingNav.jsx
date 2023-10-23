d5736977a0edde90b8232424c0b346e657816d8a
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-10-23 17:51:36 +0500

    Floating Navbar component

--- src/sections/floating-nav/FloatingNav.jsx
+++ src/sections/floating-nav/FloatingNav.jsx
@@ -1,7 +1,26 @@
+import data from './data';
+import Scrollspy from 'react-scrollspy';
+import Nav from './Nav';
+
 import './floating-nav.css';
 
 const FloatingNav = () => {
-  return <section id="floating__nav"></section>;
+  return (
+    <section id="floating__nav">
+      <ul id="floating__nav">
+        <Scrollspy
+          offset={-500}
+          className="scrollspy"
+          items={['header', 'about', 'services', 'portfolio', 'contact']}
+          currentClassName="active"
+        >
+          {data.map((item) => (
+            <Nav key={item.id} item={item} />
+          ))}
+        </Scrollspy>
+      </ul>
+    </section>
+  );
 };
 
 export default FloatingNav;
